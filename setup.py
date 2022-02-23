927cf4614eb6c476e65d6a1e6d0dc7b7baf032bd
p064033 <jef.vantongerloo@ap.be>
2022-02-23 20:34:48 +0100

    license naming fix

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 config = {
     "name": "textfsm_aos",
-    "version": "0.1.0",
+    "version": "1.1.2",
     "description": "Alcatel-Lucent Enterprise AOS CLI parsing (TextFSM) ",
     "long_description": README,
     "long_description_content_type": "text/markdown",
     "author": "Jef Vantongerloo",
     "url": "https://github.com/jefvantongerloo/textfsm-aos",
     "download_url": "",
-    "license": "Apache 2.0",
+    "license": "Apache-2.0",
     "author_email": "jefvantongerloo@gmail.com",
     "include_package_data": True,
     "install_requires": ["scrapli", "textfsm"],
