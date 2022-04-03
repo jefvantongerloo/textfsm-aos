7a2aae8d26ebe329be89f62d0bc5ac8963986e95
jefvantongerloo <jefvantongerloo@gmail.com>
2022-04-03 20:41:41 +0200

    add pytest test index count

--- tests/test_parsed_data.py
+++ tests/test_parsed_data.py
@@ -1,7 +1,10 @@
 """Pytests for TextFSM_aos project."""
-import textfsm_aos
+
 import yaml
 import pytest
+import textfsm_aos
+from textfsm_aos import templates
+import importlib.resources as pkg_resources
 
 
 def _get_raw(platform: str, command: str) -> str:
@@ -40,3 +43,26 @@ def test_parsed_data(template: dict):
     print("Benchmark data: \n {0} \n".format(benchmark_data))
 
     assert parsed_data == benchmark_data
+
+
+def test_template_index_count():
+    len_template_files = 0
+
+    template_index = textfsm_aos.parser._get_template_index()
+    len_template_index = len(template_index)
+
+    template_files = pkg_resources.contents(templates)
+    for template in template_files:
+        if template.find("ale_") != -1:
+            len_template_files += 1
+
+    assert len_template_files == len_template_index
+
+
+# def test_template_index_names():
+#     template_index = textfsm_aos.parser._get_template_index()
+
+#     template_files = pkg_resources.contents(templates)
+#     for template in template_files:
+
+#     assert len_template_files == len_template_index
