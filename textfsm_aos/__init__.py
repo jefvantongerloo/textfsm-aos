dace522e2caedbf798ff6c2cba1b2e0a1db14718
Jef Vantongerloo <jefvantongerloo@gmail.com>
2023-06-27 14:17:50 +0200

    version 1.1.3

--- textfsm_aos/__init__.py
+++ textfsm_aos/__init__.py
@@ -1,5 +1,5 @@
 """Textfsm_aos - Parse scli output and return structured data."""
 from textfsm_aos import parser  # noqa
 
-__version__ = "1.1.1"
+__version__ = "1.1.3"
 __author__ = "Jef Vantongerloo"
